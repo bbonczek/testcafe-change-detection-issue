5dc7923a3bf380fc37ab98fd4b83cda42453cb0b
Bartosz Baczek <bbaczek@rockwellautomation.com>
2019-01-18 11:43:06 +0100

    simplified code a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,9 @@ import { Observable } from 'rxjs';
 @Component({
     selector: 'app-root',
     template: `
-    <div class="container">
         <h3>Resize page, to check whether scren width is bigger than 800px, or not</h3>
         <h1>{{isScreenBigEnough | async}}</h1>
-    </div>
-    `,
-    styles: [`
-        .container {
-            height: 550px
-        }
-
-        .sidenav {
-            width: 180px;
-        }
-
-        p {
-            cursor: pointer;
-            height: 48px;
-            line-height: 48px;
-            padding-left: 16px;
-            padding-right: 16px;
-        }
-
-        .inner-container {
-            height: 500px;
-        }
-    `]
+    `
 })
 export class AppComponent {
     private isScreenBigEnough: Observable<boolean>;
